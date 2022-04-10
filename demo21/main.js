9bff2957ecadd6c49d7add63ee2c3d9fd1bff090
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-10 13:36:06 +0800

    perf: ⚡️ use @lcdp/offline-plugin/runtime register sw.js

--- demo21/main.js
+++ demo21/main.js
@@ -1,12 +1,20 @@
 require('./main.css');
-// const runtime = require('serviceworker-webpack-plugin/lib/runtime');
 
-if (navigator.serviceWorker) {
-  window.addEventListener('DOMContentLoaded',function() {
-    // 调用 serviceWorker.register 注册，参数 /sw.js 为脚本文件所在的 URL 路径
-    navigator.serviceWorker.register('sw.js');
-  });
-}
+var OfflinePlugin = require('@lcdp/offline-plugin/runtime');
 
-// require('offline-plugin/runtime').install();
+OfflinePlugin.install({
+  onInstalled: function() {
+    console.log('onInstalled');
+  },
+  onUpdating: function() {
+    console.log('onUpdating');
+  },
+  onUpdateReady: function() {
+    OfflinePlugin.applyUpdate();
+  },
+  onUpdated: function() {
+    window.location.reload();
+  }
+});
+
 window.document.getElementById('app').innerText = 'Hello,Webpack';
