449683166807042bbdf6e450f56af8ea9cf4153c
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-11 23:28:27 +0800

    feat: 🎸 add demo31

---
+++ demo31/main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { createRoot } from 'react-dom/client';
+import './main.css';
+
+class Button extends Component {
+  render() {
+    return <h1>Hello,Webpack</h1>
+  }
+}
+
+const container = window.document.getElementById('app');
+const root = createRoot(container);
+// 在 Root 节点上渲染根组件
+root.render(<Button />);
