82a1e4e802705f894090ca1573ec1d99084d60e6
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-11 22:08:32 +0800

    feat: 🎸 add demo30

---
+++ demo30/main.js
@@ -0,0 +1,17 @@
+// import 'core-js/actual/object/assign';
+// import 'core-js/actual/promise';
+// import 'whatwg-fetch';
+
+import React, { Component } from 'react';
+import { createRoot } from 'react-dom/client';
+
+class Button extends Component {
+  render() {
+    return <h1>Hello,Webpack</h1>
+  }
+}
+
+const container = window.document.getElementById('app');
+const root = createRoot(container);
+// 在 Root 节点上渲染根组件
+root.render(<Button />);
