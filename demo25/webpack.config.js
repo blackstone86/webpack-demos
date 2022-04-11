295d7e013a3b1452955230ea608271fae64105cb
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-11 14:13:54 +0800

    feat: 🎸 add demo25

---
+++ demo25/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const { WebPlugin } = require('web-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // JS 执行入口文件
+  entry: {
+    main: './main.js'
+  },
+  output: {
+    // 把所有依赖的模块合并输出到一个 bundle.js 文件
+    filename: '[name]_[chunkhash:8].js',
+    // 输出文件都放到 dist 目录下
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // 提取出 Chunk 中的 CSS 代码到单独的文件中 且 压缩 CSS 代码
+        use: [
+          MiniCssExtractPlugin.loader, 
+          // 转换 .css 文件需要使用的 Loader
+          "css-loader"
+        ],
+      },
+      {
+        test: /\.png$/,
+        type: 'asset/inline',
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `[name]_[contenthash:8].css`,
+    }),
+    new HtmlWebpackPlugin({
+      inject: false,
+      template: 'template.ejs', // HTML 模版文件所在的文件路径 https://github.com/jaketrent/html-webpack-template
+      appMountIds: ['app'],
+    }),
+  ]
+};
