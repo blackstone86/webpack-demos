c8c08fd5f0d383c68ea327f15336985402b77d05
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-12 17:25:49 +0800

    feat: 🎸 add demo32

---
+++ demo32/sub.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { createRoot } from 'react-dom/client';
+import './main.css';
+
+class Button extends Component {
+  render() {
+    return <h1>Hello,Webpack5 From sub.js</h1>
+  }
+}
+
+const container = window.document.getElementById('sub');
+const root = createRoot(container);
+// 在 Root 节点上渲染根组件
+root.render(<Button />);
