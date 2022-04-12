c3f68b2b5ce631018eabe9cb6bbac1d11ddb042a
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-12 21:31:05 +0800

    fix: 🐛 add demo35

---
+++ demo35/webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+// const { WebPlugin } = require('web-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+module.exports = {
+  entry: {
+    app: './main.js'// Chunk app 的 JS 执行入口文件
+  },
+  output: {
+    filename: '[name]_[chunkhash:8].js',// 给输出的文件名称加上 hash 值
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: ['babel-loader'],
+        // 排除 node_modules 目录下的文件，node_modules 目录下的文件都是采用的 ES5 语法，没必要再通过 Babel 去转换
+        exclude: path.resolve(__dirname, 'node_modules'),
+      },
+      {
+        test: /\.css/,// 增加对 CSS 文件的支持
+        // 提取出 Chunk 中的 CSS 代码到单独的文件中 且 压缩 CSS 代码
+        use: [
+          MiniCssExtractPlugin.loader, 
+          // 转换 .css 文件需要使用的 Loader
+          "css-loader"
+        ],
+      },
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new CssMinimizerPlugin(),
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ // 一个 HtmlWebpackPlugin 对应一个 HTML 文件
+      inject: false,
+      template: 'template.ejs', // HTML 模版文件所在的文件路径 https://github.com/jaketrent/html-webpack-template
+      filename: `index.html`, // 输出的 HTML 的文件名称
+      appMountIds: ['app'],
+      chunks: ['app'],
+    }),
+    new MiniCssExtractPlugin({
+      filename: `[name]_[contenthash:8].css`, // 给输出的 CSS 文件名称加上 hash 值
+    }),
+  ],
+};
