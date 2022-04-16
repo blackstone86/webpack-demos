f47da26629e703ea634dafbb54607a7258fb74dd
jimhuang <jimhuang@digitalgd.com.cn>
2022-04-16 12:17:13 +0800

    fix: 🐛 Update demo16-18
    fix demo18 server side rendering 404

--- demo18/http_server.js
+++ demo18/http_server.js
@@ -5,16 +5,14 @@ const app = express();
 // 调用构建出的 bundle_server.js 中暴露出的渲染函数，再拼接下 HTML 模版，形成完整的 HTML 文件
 app.get('/', function (req, res) {
   res.send(`
-<html>
-<head>
-  <meta charset="UTF-8">
-</head>
-<body>
-<div id="app">${render()}</div>
-<!--导入 webpack 输出的用于浏览器端渲染的 JS 文件-->
-<script src="./dist/bundle_browser.js"></script>
-</body>
-</html>
+    <html>
+    <head>
+      <meta charset="UTF-8">
+    </head>
+    <body>
+    <div id="app">${render()}</div>
+    </body>
+    </html>
   `);
 });
 
--- demo18/webpack.config.js
+++ demo18/webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   // JS 执行入口文件
-  entry: './main_browser.js',
+  entry: {
+    main: './main_browser.js',
+  },
   output: {
     // 把所有依赖的模块合并输出到一个 bundle.js 文件
-    filename: 'bundle_browser.js',
+    filename: '[name]_[chunkhash:8].js',
     // 输出文件都放到 dist 目录下
     path: path.resolve(__dirname, './dist'),
   },
@@ -27,5 +30,14 @@ module.exports = {
       },
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({ // 一个 HtmlWebpackPlugin 对应一个 HTML 文件
+      inject: false,
+      template: 'template.ejs', // HTML 模版文件所在的文件路径 https://github.com/jaketrent/html-webpack-template
+      filename: `index.html`, // 输出的 HTML 的文件名称
+      chunks: ['main'],
+      appMountIds: ['app']
+    })
+  ],
   devtool: 'source-map' // 输出 source-map 方便直接调试 ES6 源码
 };
